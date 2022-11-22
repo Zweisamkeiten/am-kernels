0e4beb0374983e7788c382be622cd495b7fbea6e
Einsam <snitxmhm@gmail.com>
2022-11-22 14:28:38 +0800

    test(klib-tests): test_memmove()

--- tests/klib-tests/src/tests/write_test.c
+++ tests/klib-tests/src/tests/write_test.c
@@ -35,8 +35,30 @@ void test_memcpy() {
   }
 }
 
+void test_memmove() {
+  int l, r;
+  for (l = 0; l < N; l ++) {
+    for (r = l + 1; r <= N; r ++) {
+      reset();
+      srand(l);
+      int src_length = rand() % (r - l);
+      uint8_t src[src_length];
+      for (int i = 0; i < src_length; i++) {
+        src[i] = rand() % 256;
+      }
+      memcpy(data + l, src, src_length);
+      check_seq(0, l, 1);
+      for (int j = l; j < l + src_length; j++) {
+        check_eq(j, j+1, src[j - l]);
+      };
+      check_seq(r, N, r + 1);
+    }
+  }
+}
+
 void test_write() {
   test_memset();
   test_memcpy();
+  test_memmove();
 }
 
