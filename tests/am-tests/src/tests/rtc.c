235cd44c61d75997d3e1003f59b997fa2d1bc2a3
Einsam <snitxmhm@gmail.com>
2022-12-03 20:26:28 +0800

    bugtest(nemu: timer): nemu中rtc实现bug, 复现示例

--- tests/am-tests/src/tests/rtc.c
+++ tests/am-tests/src/tests/rtc.c
@@ -1,17 +1,25 @@
 #include <amtest.h>
+#include <klib.h>
+#include <limits.h>
+
+static uint64_t uptime() { return io_read(AM_TIMER_UPTIME).us; }
 
 void rtc_test() {
-  AM_TIMER_RTC_T rtc;
-  int sec = 1;
+  // while(1) printf("us: %lu\n", uptime());
+  // AM_TIMER_RTC_T rtc;
+  // int sec = 1;
   while (1) {
-    while(io_read(AM_TIMER_UPTIME).us / 1000000 < sec) ;
-    rtc = io_read(AM_TIMER_RTC);
-    printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
-    if (sec == 1) {
-      printf("%d second).\n", sec);
-    } else {
-      printf("%d seconds).\n", sec);
-    }
-    sec ++;
+    printf("before uptime: %lu\n", uptime());
+    long int i = 0;
+    while(i < 10000000) i++;
+    printf("after uptime: %lu\n", uptime());
+    // rtc = io_read(AM_TIMER_RTC);
+    // printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
+    // if (sec == 1) {
+    //   printf("%d second).\n", sec);
+    // } else {
+    //   printf("%d seconds).\n", sec);
+    // }
+    // sec ++;
   }
 }
